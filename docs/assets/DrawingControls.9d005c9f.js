638aa430e3882ca90955d7d9bbc567fc847f5efa
NoboNobo <irieda@gmail.com>
2022-11-11 08:59:48 +0900

    base to /hobby2job-presentation/

--- docs/assets/DrawingControls.9d005c9f.js
+++
@@ -1,1 +0,0 @@
-import"./DrawingControls.vue_vue_type_script_setup_true_lang.b60c761c.js";import{_ as t}from"./DrawingControls.vue_vue_type_script_setup_true_lang.b60c761c.js";import"./index.2ea9a5c4.js";export{t as default};
--- docs/assets/DrawingPreview.e5fed904.js
+++
@@ -1,1 +0,0 @@
-import"./index.2ea9a5c4.js";import{al as r}from"./index.2ea9a5c4.js";export{r as default};
